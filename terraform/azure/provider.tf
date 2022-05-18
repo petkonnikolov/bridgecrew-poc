e260983f4493a1fece154515006460ace338d52e
petkonnikolov <nikolov_p@hotmail.com>
2022-05-18 15:27:33 +0300

    be config 2

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,10 +1,15 @@
 terraform {
-  backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.6.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name = "tfstate"
+    storage_account_name = "tfstate18119"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
   }
 }
 
